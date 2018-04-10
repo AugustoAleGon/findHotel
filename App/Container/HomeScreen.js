5c5f6989c13e5cbe4ac59b5269a7807169369f92
augustoalegon <mac@MacBook-Pro-de-Mac.local>
2018-04-09 23:02:33 -0500

    Standart convention fix

--- App/Container/HomeScreen.js
+++ App/Container/HomeScreen.js
@@ -1,25 +1,29 @@
 import React, {Component} from 'react'
 import {
-    View,
-    Text,
-    TouchableOpacity
+  View,
+  Text,
+  TouchableOpacity
 } from 'react-native'
 
+import styles from './Styles/HomeScreenStyle'
+
 class HomeScreen extends Component {
-    render () {
-        return(
-            <View style={styles.homeContainer}>
-                <Text>This is my home Screen!</Text>
-                <TouchableOpacity style={{
-                    backgroundColor: 'orange'
-                }}>
-                    <Text>
-                        Go to the next Screen!
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+  render () {
+    return (
+      <View style={styles.homeContainer}>
+        <Text>This is my home Screen!</Text>
+        <TouchableOpacity style={{
+          backgroundColor: 'orange',
+          height: 100,
+          width: 100
+        }}>
+          <Text>
+              Go to the next Screen!
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 export default HomeScreen
--- App/Container/Styles/HomeScreenStyle.js
+++ App/Container/Styles/HomeScreenStyle.js
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
-    homeContainer: {
-        backgroundColor: 'yellow'
-    }
+  homeContainer: {
+    backgroundColor: 'yellow'
+  }
 })
